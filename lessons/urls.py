d55f0d68f7fb3f553e77acae1227a36c47c7701a
Brandmerle <brandon.merle@yahoo.com>
2025-05-27 20:54:21 -0400

    527

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('list/', views.NoteList.as_view(), name="note_list"),
+    path('create/', views.NoteCreate.as_view(), name="note_create"),
 
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Note
+from .forms import NoteForm
+from django.urls import reverse_lazy
+
 
 """
 Class-based views:
@@ -18,3 +21,9 @@ LoginView   = login
 class NoteList(ListView):
     model = Note
     template_name = "lessons/list.html"
+
+class NoteCreate(CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "lessons/create.html"
+    success_url = reverse_lazy('note_list')
